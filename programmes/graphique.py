b88f6fb605d8d4c635f8a23ee1237ff08202a6b9
Lise-Brisset <lise.brisset9@gmail.com>
2024-04-05 11:06:45 +0200

    feat: ajout du dossier programmes/

---
+++ programmes/graphique.py
@@ -0,0 +1,3 @@
+""" Programme qui servira à afficher nos données annotées sous forme de graphique.
+On pourra montrere de manière statistique la présence d'étiquette de premier niveau et quelle étiquette sont présentent au niveau , etc.
+"""
