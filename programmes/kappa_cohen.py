189be029604f848bc01652ef170d54fe0f0d8d87
PatriciaAugustyn <patricia.augustyn@sorbonne-nouvelle.fr>
2024-04-06 19:13:27 +0200

    feat: modification du script kappa_cohen.py et ajout du graphique niv1 et niv2

--- programmes/kappa_cohen.py
+++ programmes/kappa_cohen.py
@@ -12,15 +12,14 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv("../annotation/niveau1.csv")
 
+# On supprime les lignes qui n'ont aucune valeur
+data.dropna(inplace=True)
+
 # On prend les colonnes de chaque annotateur
 lise = data["Lise"]
 solomiia = data["Solomiia"]
 patricia = data["Patricia"]
 
-# On supprime les lignes qui n'ont aucune valeur
-lise.dropna(inplace=True)
-solomiia.dropna(inplace=True)
-patricia.dropna(inplace=True)
 
 # On calcul le Kappa de Cohen entre paire d'annotateurs
 kappa_lise_solomiia = cohen_kappa_score(lise, solomiia)
@@ -41,11 +40,11 @@ plt.figure(figsize=(8, 6))
 plt.bar(resultats["Annotateurs"], resultats["Kappa de Cohen"], color=colors)
 plt.xlabel("Paires d'annotateurs")
 plt.ylabel("Kappa de Cohen")
-plt.title("Kappa de Cohen entre chaque paire d'annotateurs")
+plt.title("Kappa de Cohen entre chaque paire d'annotateurs : Niveau 1")
 plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
 
 # On enregistre le graphique dans le dossier graphiques et on l'affiche
-plt.savefig('../graphiques/kappa_cohen.png')
+plt.savefig('../graphiques/kappa_cohen_niv1.png')
 
 plt.show()
