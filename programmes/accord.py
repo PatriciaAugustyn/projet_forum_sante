8df48fa65d580c29baecef23b3a2be8737962e3d
Lise-Brisset <lise.brisset9@gmail.com>
2024-04-28 15:43:43 +0200

    fix: suppr d'un programme vide inutile

--- programmes/accord.py
+++
@@ -1,3 +0,0 @@
-""" Ce programme servira à calculer l'accord inter annotateur entre nos trois annotations.
-Il pourra aussi aficher ces informations sous forme de graphique pour une jolie visualisation des résultats.
-"""
